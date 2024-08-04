417162c70cbbca1bd48493b34b79692108d95c79
Anusha12-vijay <anushavijay1202@gmail.com>
2024-08-04 20:50:03 +0530

    connected database

---
+++ controllers/users_controller.js
@@ -0,0 +1,50 @@
+const User=require('../models/users')
+
+
+module.exports.profile=function(req,res){
+    res.end('<h1>User Profile</h1>');
+
+}
+
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title:"SocialNet | Sign Up"
+    })
+}
+
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title:"SocialNet | Sign In"
+    })
+    
+}
+
+// get the sign
+
+module.exports.create = async function(req, res) {
+    // Check if passwords match
+    if (req.body.password !== req.body.confirm_password) {
+        return res.redirect('back');
+    }
+
+    try {
+        // Find the user by email
+        let user = await User.findOne({ email: req.body.email });
+
+        // If the user doesn't exist, create a new user
+        if (!user) {
+            user = await User.create(req.body);
+            return res.redirect('/users/sign-in');
+        } else {
+            return res.redirect('back');
+        }
+    } catch (err) {
+        console.error('Error in finding or creating user during sign up:', err);
+        return res.redirect('back');
+    }
+};
+
+
+module.exports.createSession=function(req,res){
+    //TODO Later
+}
--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express= require('express');
+const cookieParser =require('cookie-parser')
 const  app=express();
 const port= 8000;
+const expressLayouts =require('express-ejs-layouts');
+const db=require('./config/mongoose');
 
-
+app.use(express.urlencoded());
+app.use(cookieParser());
 //use express router
 app.use('/',require('./routes'));
+
+//set up views engine
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.use(express.static('./assets'));
+//extract style and scripts from sub pages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 
 
