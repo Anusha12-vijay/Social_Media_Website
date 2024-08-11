37fe54547d0ea20ac45365ca46e732d9eacb7599
Anusha12-vijay <anushavijay1202@gmail.com>
2024-08-11 12:42:11 +0530

    update

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,8 @@ router.get('/sign-up',usersController.signUp)
 router.get('/sign-in',usersController.signIn)
 
 router.post('/create',usersController.create)
+//sign out
+
 //use passport as the middleware to authenticate
 
 router.post('/create-session',passport.authenticate(
